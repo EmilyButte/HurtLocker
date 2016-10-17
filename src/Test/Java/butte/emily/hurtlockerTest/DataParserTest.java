4dbfd43d1ba4eea49ceaf27eb702e33c0a5b42f4
Emily Butte <emily.butte@gmail.com>
2016-10-17 14:36:39 -0400

    begin mapping out strategy

--- src/Test/Java/butte/emily/hurtlockerTest/DataParserTest.java
+++
@@ -1,7 +0,0 @@
-package butte.emily.hurtlockerTest;
-
-/**
- * Created by emilybutte on 10/17/16.
- */
-public class DataParserTest {
-}
---
+++ src/main/java/butte/emily/hurtlocker/CustomException.java
@@ -0,0 +1,15 @@
+package butte.emily.hurtlocker;
+
+/**
+ * Created by emilybutte on 10/17/16.
+ */
+public class CustomException extends Exception {
+
+//    String data;
+//
+//    public CustomException(String data) {
+//        this.data = data;
+//    }
+
+
+}
